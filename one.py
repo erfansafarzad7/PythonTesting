d2a844f943ceed3c81f79ffef1cb65aad88c063e
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-07-03 14:27:27 +0430

    SetUp-Teardown

--- one.py
+++ one.py
@@ -1,3 +1,4 @@
+
 # Doc Test
 # python -m doctype one.py
 # python -m doctype -v one.py
@@ -17,17 +18,36 @@
 
 # ----------------------------------------------------
 
+"""
+L 2
+"""
+
 # Unit Test
 # python -m unittest test_one.py
 # python -m unittest discover ( run all test_ in directory )
 
-def add(x, y):
-    return x + y
+# def add(x, y):
+#     return x + y
+#
+#
+# def division(x, y):
+#     if y == 0:
+#         raise ZeroDivisionError('cant divide by zero !')
+#     return x // y
+
+# ----------------------------------------------------
+"""
+L 3
+"""
 
 
-def division(x, y):
-    if y == 0:
-        raise ZeroDivisionError('cant divide by zero !')
-    return x // y
+class Person:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def fullname(self):
+        return f"{self.fname} {self.lname}"
+
 
 # ----------------------------------------------------
--- test_one.py
+++ test_one.py
@@ -1,16 +1,44 @@
 import unittest
 import one
 
+"""
+L 2
+"""
+# class OneTest(unittest.TestCase):
+#     # func name must starts with 'test_'
+#     def test_add(self):
+#         self.assertEqual(one.add(1, 2), 3)
+#         self.assertEqual(one.add(-2, 3), 1)
+#
+#     def test_division(self):
+#         self.assertEqual(one.division(10, 2), 5)
+#         self.assertRaises(ZeroDivisionError, one.division, 5, 0)
 
-class OneTest(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(one.add(1, 2), 3)
-        self.assertEqual(one.add(-2, 3), 1)
 
-    def test_division(self):
-        self.assertEqual(one.division(10, 2), 5)
-        self.assertRaises(ZeroDivisionError, one.division, 5, 0)
+# ----------------------------------------------------
+"""
+L 3
+"""
 
+
+class PersonTest(unittest.TestCase):
+    # before start testing
+    def setUp(self):
+        self.p1 = one.Person('erfan', 'safarzad')
+
+    # after testing done
+    def tearDown(self):
+        print('Done..')
+
+    def test_fullname(self):
+        self.assertEqual(self.p1.fullname(), 'erfan safarzad')
+
+# ----------------------------------------------------
+
+
+# ----------------------------------------------------
 
 if __name__ == '__main__':
     unittest.main()
+
+# ----------------------------------------------------
