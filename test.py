55c1bec7647c93c43a39cf4c250d2835ac6d278c
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-07-03 12:31:43 +0430

    Start-DocTest

---
+++ test.py
@@ -0,0 +1,18 @@
+# Doc Test
+# python -m doctype test.py
+# python -m doctype -v test.py
+
+"""
+>>> add(2, 3)
+5
+"""
+
+
+def add(x, y):
+    """
+    >>> add(1, 2)
+    3
+    """
+    return x + y
+
+# ----------------------------------------------------
